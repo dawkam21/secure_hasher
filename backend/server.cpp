e2c8f9548669f646b3b47b815c1046d2120f8a2b
dawkam21 <dawid.jan.kaminski@gmail.com>
2026-02-08 12:45:05 +0100

    profesjonalne hashowanie w c++ po stronie serwera

--- backend/server.cpp
+++ backend/server.cpp
@@ -1,4 +1,38 @@
 #include "crow_all.h"
+#include <openssl/evp.h> // biblioteka OpenSSL
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+std::string sha256(const std::string str) {
+    // I - tworzę kontekst dla algorytmu
+    EVP_MD_CTX* context = EVP_MD_CTX_new();
+
+    // II - wybieramy algorytm (sha256)
+    const EVP_MD* md = EVP_sha256();
+
+    // III - inicjalizacja operacji
+    EVP_DigestInit_ex(context, md, nullptr);
+
+    // IV - przetwarzanie danych
+    EVP_DigestUpdate(context, str.c_str(), str.size());
+
+    // V - pobieranie wyniku (hash ma zawsze 32 bajty dla SHA-256)
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int lengthOfHash = 0;
+    EVP_DigestFinal_ex(context, hash, &lengthOfHash);
+
+    // VI - sprzątanie pamięci (ważne w C++)
+    EVP_MD_CTX_free(context);
+
+    // VII - konwersja na czytelny hex (16tkowy)
+    std::stringstream ss;
+    for(unsigned int i = 0; i < lengthOfHash; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+
+    return ss.str();
+}
 
 int main() {
     crow::SimpleApp app;
@@ -39,10 +73,14 @@ int main() {
             }
         }
 
+        std::string hashed_password = sha256(pass);
+
         // tworzę odpowiedź
         crow::json::wvalue z;
-        z["hash"] = strength;
-        
+        z["hash"] = hashed_password;
+        z["strength"] = strength; // można wysłać oba pola (?)
+        z["algorithm"] = "SHA-256 (OpenSSL 3.0 EVP)";
+
         // zwraca czysty JSON (CORS mnie nie obchodzi, bo mam Proxy)
         return crow::response(z);
     });
